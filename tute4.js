619fa290734776998b9e8890e6703351a52a7f5a
jddeepu <dg6732049@gmail.com>
2023-07-08 14:35:32 +0530

    all file added

---
+++ tute4.js
@@ -0,0 +1,29 @@
+
+// about function
+function greet(name, greetText) {
+    console.log(greetText + " " + name);
+    console.log(name + "is a good boy");
+}
+
+let name = "deepu";
+let name1 = "ravi";
+let name2 = "vikash";
+let greetText = "morning"
+greet(name, greetText);
+greet(name1, greetText);
+greet(name2, greetText);
+
+
+/*
+console.log(name +" " +"is a honest man");
+console.log(name1+ " "+ "is a honest man");
+console.log(name2 + " "+ "is a honest man");
+*/
+//return function
+/*
+function sum(a,b,c){
+   let d= a*b*c;
+    return d;//returvn ke bad kuchh likh raha hu to kam nahi karega 
+}
+let retun=sum(1,2,3);
+console.log(retun);*/
